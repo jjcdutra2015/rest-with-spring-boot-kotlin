8c11485ab5d6cd91705a5e99f09a797da2b0c180
juliodutra <jjcdutra@gmail.com>
2023-12-02 09:24:18 -0300

    Adicionar Person Repository

--- src/main/kotlin/com/jjcdutra/model/Person.kt
+++ src/main/kotlin/com/jjcdutra/model/Person.kt
@@ -1,9 +1,24 @@
 package com.jjcdutra.model
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "person")
 data class Person(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0,
+
+    @Column(name = "first_name", nullable = false, length = 80)
     val firstName: String = "",
+
+    @Column(name = "last_name", nullable = false, length = 80)
     val lastName: String = "",
+
+    @Column(nullable = false, length = 100)
     val address: String = "",
+
+    @Column(nullable = false, length = 6)
     val gender: String = ""
 )
