a55f1a2eba9812e505cdd5ab66db693b691b5496
juliodutra <jjcdutra@gmail.com>
2023-12-18 20:25:50 -0300

    Configuração inicial do swagger

---
+++ src/main/kotlin/com/jjcdutra/config/OpenApiConfig.kt
@@ -0,0 +1,28 @@
+package com.jjcdutra.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun customOpenApi(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("RESTful API with Kotlin and Spring Boot")
+                    .version("v1")
+                    .description("Some description about your API.")
+                    .termsOfService("Terms of services")
+                    .license(
+                        License()
+                            .name("Apache 2.0")
+                            .url("www.license.com")
+                    )
+            )
+    }
+}
