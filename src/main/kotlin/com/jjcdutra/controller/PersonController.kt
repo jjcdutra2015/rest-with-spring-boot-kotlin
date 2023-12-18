bef815341562e45d3bf305340f55fee6544105f4
juliodutra <jjcdutra@gmail.com>
2023-12-18 20:48:20 -0300

    Customizando a documentação do swagger

--- src/main/kotlin/com/jjcdutra/controller/PersonController.kt
+++ src/main/kotlin/com/jjcdutra/controller/PersonController.kt
@@ -2,6 +2,12 @@ package com.jjcdutra.controller
 
 import com.jjcdutra.data.vo.v1.PersonVO
 import com.jjcdutra.service.PersonService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -9,17 +15,76 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "Endpoints for Managing People")
 class PersonController {
 
     @Autowired
     private lateinit var service: PersonService
 
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Operation(
+        summary = "Finds all People", description = "Finds all People", tags = ["People"],
+        responses = [
+            ApiResponse(
+                description = "Success", responseCode = "200",
+                content = [Content(array = ArraySchema(schema = Schema(implementation = PersonVO::class)))]
+            ),
+            ApiResponse(
+                description = "No Content", responseCode = "204",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Bad Request", responseCode = "400",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Unauthorized", responseCode = "401",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Not Found", responseCode = "404",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Internal Error", responseCode = "500",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            )
+        ]
+    )
     fun findAll(): List<PersonVO> {
         return service.findAll()
     }
 
     @GetMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Operation(
+        summary = "Finds a Person", description = "Finds a Person", tags = ["People"],
+        responses = [
+            ApiResponse(
+                description = "Success", responseCode = "200",
+                content = [Content(schema = Schema(implementation = PersonVO::class))]
+            ),
+            ApiResponse(
+                description = "No Content", responseCode = "204",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Bad Request", responseCode = "400",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Unauthorized", responseCode = "401",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Not Found", responseCode = "404",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Internal Error", responseCode = "500",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            )
+        ]
+    )
     fun findById(@PathVariable(value = "id") id: Long): PersonVO {
         return service.findById(id)
     }
@@ -27,6 +92,27 @@ class PersonController {
     @PostMapping(
         consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    @Operation(
+        summary = "Adds a new Person", description = "Adds a new Person", tags = ["People"],
+        responses = [
+            ApiResponse(
+                description = "Success", responseCode = "200",
+                content = [Content(schema = Schema(implementation = PersonVO::class))]
+            ),
+            ApiResponse(
+                description = "Bad Request", responseCode = "400",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Unauthorized", responseCode = "401",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Internal Error", responseCode = "500",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            )
+        ]
     )
     fun create(@RequestBody person: PersonVO): PersonVO {
         return service.create(person)
@@ -36,11 +122,65 @@ class PersonController {
         consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE]
     )
+    @Operation(
+        summary = "Updates a person's information", description = "Updates a person's information", tags = ["People"],
+        responses = [
+            ApiResponse(
+                description = "Success", responseCode = "200",
+                content = [Content(schema = Schema(implementation = PersonVO::class))]
+            ),
+            ApiResponse(
+                description = "No Content", responseCode = "204",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Bad Request", responseCode = "400",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Unauthorized", responseCode = "401",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Not Found", responseCode = "404",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Internal Error", responseCode = "500",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            )
+        ]
+    )
     fun update(@RequestBody person: PersonVO): PersonVO {
         return service.update(person)
     }
 
     @DeleteMapping(value = ["/{id}"])
+    @Operation(
+        summary = "Deletes a person", description = "Deletes a person", tags = ["People"],
+        responses = [
+            ApiResponse(
+                description = "No Content", responseCode = "204",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Bad Request", responseCode = "400",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Unauthorized", responseCode = "401",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Not Found", responseCode = "404",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            ),
+            ApiResponse(
+                description = "Internal Error", responseCode = "500",
+                content = [Content(schema = Schema(implementation = Unit::class))]
+            )
+        ]
+    )
     fun delete(@PathVariable(value = "id") id: Long): ResponseEntity<*> {
         service.delete(id)
         return ResponseEntity.noContent().build<Any>()
