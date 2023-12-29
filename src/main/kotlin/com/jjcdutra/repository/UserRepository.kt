0a579893718e66aa12fe4ef53fdf831879b3f02b
juliodutra <jjcdutra@gmail.com>
2023-12-29 16:01:27 -0300

    Implementando repositorio e service de User

---
+++ src/main/kotlin/com/jjcdutra/repository/UserRepository.kt
@@ -0,0 +1,14 @@
+package com.jjcdutra.repository
+
+import com.jjcdutra.model.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User?, Long?> {
+
+    @Query("SELECT u FROM User u WHERE u.userName =:userName")
+    fun findByUserName(@Param("userName") userName: String?): User?
+}
