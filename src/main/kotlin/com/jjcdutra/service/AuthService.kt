1f68fce1b6636ba5c8604cbb1269089e8bac13e4
juliodutra <jjcdutra@gmail.com>
2024-01-04 18:44:18 -0300

    Implementando serviço responsável pela autenticação

---
+++ src/main/kotlin/com/jjcdutra/service/AuthService.kt
@@ -0,0 +1,46 @@
+package com.jjcdutra.service
+
+import com.jjcdutra.data.vo.v1.AccountCredentialsVO
+import com.jjcdutra.data.vo.v1.TokenVO
+import com.jjcdutra.repository.UserRepository
+import com.jjcdutra.security.jwt.JwtTokenProvider
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+import java.util.logging.Logger
+
+@Service
+class AuthService {
+
+    @Autowired
+    private lateinit var authenticationManager: AuthenticationManager
+
+    @Autowired
+    private lateinit var tokenProvider: JwtTokenProvider
+
+    @Autowired
+    private lateinit var repository: UserRepository
+
+    private val logger = Logger.getLogger(AuthService::class.java.name)
+
+    fun signin(data: AccountCredentialsVO): ResponseEntity<*> {
+        return try {
+            val username = data.username
+            val password = data.password
+            authenticationManager.authenticate(UsernamePasswordAuthenticationToken(username, password))
+            val user = repository.findByUserName(username)
+            val tokenResponse: TokenVO = if (user != null) {
+                tokenProvider.createAccessToken(username!!, user.roles)
+            } else {
+                throw UsernameNotFoundException("Username $username not found!")
+            }
+            ResponseEntity.ok(tokenResponse)
+        } catch (e: Exception) {
+            throw BadCredentialsException("Invalid username or password supplied!")
+        }
+    }
+}
