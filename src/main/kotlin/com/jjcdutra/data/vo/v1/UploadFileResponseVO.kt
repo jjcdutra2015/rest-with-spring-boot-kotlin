c4d66d3852e871bca1a986ae791d0905ef2e9194
juliodutra <jjcdutra@gmail.com>
2024-01-15 19:40:32 -0300

    Definindo as exceptions e response

---
+++ src/main/kotlin/com/jjcdutra/data/vo/v1/UploadFileResponseVO.kt
@@ -0,0 +1,8 @@
+package com.jjcdutra.data.vo.v1
+
+class UploadFileResponseVO(
+    val fileName: String = "",
+    val fileDownloadUri: String = "",
+    val fileType: String = "",
+    val fileSize: Long = 0
+)
