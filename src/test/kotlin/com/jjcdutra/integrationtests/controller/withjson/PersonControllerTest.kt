20c8cf32f82e31afec906030404c94f0024e2dfd
juliodutra <jjcdutra@gmail.com>
2024-01-15 18:38:53 -0300

    Teste de integracao findByName

--- src/test/kotlin/com/jjcdutra/integrationtests/controller/withjson/PersonControllerTest.kt
+++ src/test/kotlin/com/jjcdutra/integrationtests/controller/withjson/PersonControllerTest.kt
@@ -267,9 +267,45 @@ class PersonControllerTest : AbstractIntegrationTest() {
         assertEquals("Female", item2.gender)
         assertEquals(false, item2.enabled)
     }
-
     @Test
     @Order(7)
+    fun findByName() {
+        val content = given()
+            .spec(specification)
+            .contentType(TestConfigs.CONTENT_TYPE_JSON)
+            .pathParam("firstName", "ayr")
+            .queryParams(
+                "page", 0,
+                "size", 12,
+                "direction", "asc"
+            )
+            .`when`()["findPersonByName/{firstName}"]
+            .then()
+            .statusCode(200)
+            .extract()
+            .body()
+            .asString()
+
+        val wrapper = objectMapper.readValue(content, PersonWrapper::class.java)
+        val people = wrapper.embedded!!.persons
+
+        val item1 = people?.get(0)
+
+        assertNotNull(item1!!.id)
+        assertNotNull(item1.firstName)
+        assertNotNull(item1.lastName)
+        assertNotNull(item1.address)
+        assertNotNull(item1.gender)
+
+        assertEquals("Ayrton", item1.firstName)
+        assertEquals("Senna", item1.lastName)
+        assertEquals("São Paulo", item1.address)
+        assertEquals("Male", item1.gender)
+        assertEquals(true, item1.enabled)
+    }
+
+    @Test
+    @Order(8)
     fun findAllWithoutToken() {
 
         val specificationWithoutToken = RequestSpecBuilder()
